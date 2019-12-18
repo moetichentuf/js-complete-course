ecd92d3772c1e9d7b342dedb2c004e48c858ba5a
moeti <moetichentuf@hotmail.com>
2019-12-18 11:28:30 +0100

    FINISHED

--- 02-maths/04-sort-numbers/script.js
+++ 02-maths/04-sort-numbers/script.js
@@ -14,9 +14,10 @@
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("run").addEventListener("click", function() {
-
+        var points = [2, 4, 14, 10, 90, 23, 16];
+        points.sort(function(a, b){return a-b});
         // your code here
-
+    document.write(points);
     });
 
 })();
