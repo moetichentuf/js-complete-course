2cbafe609df9b0220003633b5d752ba86f54ce3b
moeti <moetichentuf@hotmail.com>
2019-12-18 16:31:20 +0100

    FINISHED

--- 03-colors/01-change-bcg-one/script.js
+++ 03-colors/01-change-bcg-one/script.js
@@ -14,9 +14,26 @@
     document.getElementById("red").addEventListener("click", function () {
 
         document.body.style.backgroundColor = "red";
-    })
+    });
 
-    
+
+
+    document.getElementById("green").addEventListener("click", function () {
+
+        document.body.style.backgroundColor = "green";
+    });
+
+
+    document.getElementById("yellow").addEventListener("click", function () {
+
+        document.body.style.backgroundColor = "yellow";
+    });
+
+
+    document.getElementById("blue").addEventListener("click", function () {
+
+        document.body.style.backgroundColor = "blue";
+    })
 
 
 
