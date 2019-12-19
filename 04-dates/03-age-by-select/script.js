fcdf2774468a7746f1a794d6376147df7e104ae4
moeti <moetichentuf@hotmail.com>
2019-12-19 16:39:37 +0100

    FINISHED

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -9,22 +9,17 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-// still working on this
-// i am a little stuck..
 (function() {
 
     document.getElementById("run").addEventListener("click", function() {
 
-        let minutes = 1000 * 60;
-        let hours = minutes * 60;
-        let days = hours * 24;
-        let years = days * 365;
 
-        let birthday = new Date(document.getElementById("dob-day").value, document.getElementById("dob-month").value, document.getElementById("dob-year").value);
+
+        let birthday = new Date(document.getElementById("dob-year").value, document.getElementById("dob-month").value, document.getElementById("dob-day").value);
         let dateNow = new Date();
-        let YearsOld = Math.round((dateNow - birthday) / years);
+        let YearsOld = ((dateNow.getTime() - birthday.getTime()) / (1000*60*60*24)) / 365;
 
-        alert("You are " + YearsOld + " years old.");
+        document.getElementById("run").innerHTML = ("You are " + YearsOld + " years old.");
 
 
     })
