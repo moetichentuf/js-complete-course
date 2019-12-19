c7130df605a7d8fcbff60dd30edb0c0d4ec2c8cb
moeti <moetichentuf@hotmail.com>
2019-12-19 12:24:04 +0100

    work in progress

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -9,8 +9,23 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+// using this stack overflow code as example to try and make it work
 (function() {
 
-    // your code here
+    function ageCount() {
+        var date1 = new Date();
+        var  dob= document.getElementById("dob").value;
+        var date2=new Date(dob);
+        var pattern = /^\d{1,2}\/\d{1,2}\/\d{4}$/; //Regex to validate date format (dd/mm/yyyy)
+        if (pattern.test(dob)) {
+            var y1 = date1.getFullYear(); //getting current year
+            var y2 = date2.getFullYear(); //getting dob year
+            var age = y1 - y2;           //calculating age
+            document.write("Age : " + age);
+            return true;
+        } else {
+            alert("Invalid date format. Please Input in (dd/mm/yyyy) format!");
+            return false;
+        }}
 
 })();
