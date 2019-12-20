f7595f19263bf7bbcca276077dec48037a24b1e6
moeti <moetichentuf@hotmail.com>
2019-12-20 10:51:45 +0100

    unfinished

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -9,8 +9,33 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
+// this isn't working yet, i'm going to start on the next assignment and try this again later
+
 (function() {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function  () {
+    let year = document.getElementById("year");
+
+
+    function Calc() {
+            let d = new Date();
+            let year = 0;
+            let month;
+
+            for (month = 0; month < 12; month++) {
+                d.setFullYear(year, month, 13);
+
+                if (d.getDay() == 5) {
+                    let ourMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "January"]
+
+
+                }
+            }
+
+        document.write(Calc)
+
+        }
+    });
 
 })();
