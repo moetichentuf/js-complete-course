488c5759749072ca078006a805ab9f5ed7794481
moeti <moetichentuf@hotmail.com>
2019-12-23 11:59:44 +0100

    work in progress

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -9,8 +9,12 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(document.getElementById("run").addEventListener("click",function() {
 
-    // your code here
-
-})();
+    var arr = [];
+    while(arr.length < 10){
+        var r = Math.floor(Math.random() * 100) + 1;
+        if(arr.indexOf(r) === -1) arr.push(r);
+    }
+    console.log(arr);
+}))();
