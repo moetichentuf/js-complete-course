b0176e817190d12b1e0a7d9724559e8800bf58f5
moeti <moetichentuf@hotmail.com>
2019-12-24 10:09:12 +0100

    FINISHED

--- 05-arrays/10-reduce-array/script.js
+++ 05-arrays/10-reduce-array/script.js
@@ -9,7 +9,7 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(document.getElementById("run").addEventListener("click", function() {
 
     var people = [
         {
@@ -89,6 +89,8 @@
         },
     ];
 
-    // your code here
+    let allPeople = people.reduce((a, b) => a + b.age, 0);
 
-})();
+console.log(allPeople);
+
+})());
