5a7e9d4c88975ec6dcdad35f479f05a1945ae38a
moeti <moetichentuf@hotmail.com>
2019-12-31 12:15:56 +0100

    Work in progress

--- 06-dom/09-match-password-one/script.js
+++ 06-dom/09-match-password-one/script.js
@@ -9,8 +9,31 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(document.getElementById("run").addEventListener("click", function myFunction() {
 
-    // your code here
+    function checkPassword(form) {
+        password1 = form.getElementById("pass-one").value;
+        password2 = form.getElementById("pass-two").value;
 
-})();
+        // If password not entered
+        if (password1 == '')
+            alert ("Please enter Password");
+
+        // If confirm password not entered
+        else if (password2 == '')
+            alert ("Please enter confirm password");
+
+        // If Not same return False.
+        else if (password1 != password2) {
+            alert ("\nPassword did not match: Please try again...");
+            return false;
+        }
+
+        // If same return True.
+        else{
+            alert("Password Match: Welcome to GeeksforGeeks!");
+            return true;
+        }
+    }
+
+})());
