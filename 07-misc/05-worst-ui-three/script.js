9e8ee5b4f2594da165a4f0846a08a5d3603f0c5b
moeti <moetichentuf@hotmail.com>
2020-01-06 16:52:26 +0100

    experimenting with code from last assignment

--- 07-misc/05-worst-ui-three/script.js
+++ 07-misc/05-worst-ui-three/script.js
@@ -11,6 +11,35 @@
 
 (function() {
 
-    // your code here
+    // getbytagname to get all buttons
+    const buttons = document.getElementsByTagName("button");
+
+    // when you press the buttons the numbers inside the buttons also change
+
+
+    // go over all buttons with for loop
+    for (let i =0; i<buttons.length; i++){
+        buttons[i].addEventListener("click", function(){
+            let minVal = buttons[i].getAttribute("data-min");
+            let maxVal = buttons[i].getAttribute("data-max");
+            let currentVal = buttons[i].innerHTML;
+            //ups value by one on every click
+            currentVal++;
+            // console.log(currentVal);
+            buttons[i].innerHTML = currentVal;
+
+            //makes sure it writes a 0 instead of deleting a number when in single digit
+            if (buttons[i].innerHTML < 10) {
+                buttons[i].innerHTML = "0" +buttons[i].innerHTML;
+                buttons.value = Math.floor(Math.random()*(499-460) + 460);
+            }
+
+            document.getElementById("target").innerHTML =
+                Math.floor(Math.random() + buttons.innerHTML
+                + (buttons[1].innerHTML)
+                + (buttons[2].innerHTML)
+                + (buttons[3].innerHTML))
+        });
+    }
 
 })();
