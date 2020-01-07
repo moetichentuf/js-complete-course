62d1c4f81098771fc7933fb3eb9b6376b5944177
moeti <moetichentuf@hotmail.com>
2020-01-07 16:50:16 +0100

    got it to work with the help from many

--- 09-fetch/01-list-to-console/script.js
+++ 09-fetch/01-list-to-console/script.js
@@ -10,19 +10,11 @@
 // You will have time to focus on it later.
 
 (() => {
-    let request = new XMLHttpRequest();
-document.getElementById("run").addEventListener("click", function  (lala) {
-
-
-// Open a new connection, using the GET request on the URL endpoint
-    request.open('GET', '../../_shared/api.json', true);
-
-    request.onload = function() {
-        // Begin accessing JSON data here
-    };
-
-// Send request
-    request.send();
-console.log(lala);
-});
+    fetch("../../_shared/api.json")
+        .then((function (response) {
+            return response.json();
+        }))
+        .then(function (data) {
+            console.log(data)
+        })
 })();
