a02043ea752b3acd0ecd051e7f281309a9e45b17
moeti <moetichentuf@hotmail.com>
2020-01-08 14:53:24 +0100

    added comments(still don't fully understand it

--- 09-fetch/03-details/script.js
+++ 09-fetch/03-details/script.js
@@ -10,37 +10,46 @@
 // You will have time to focus on it later.
 
 (() => {
+    document.getElementById('run').addEventListener('click', function() {
 
-
-    document.getElementById("run").addEventListener("click", function() {
-
+        // get json
         fetch("../../_shared/api.json")
+            .then(response => {
+                return response.json();
+            })
+            .then(heroList => {
 
-            // on click, fetch api
-            .then((function (link) {
-                return link.json();
-            }))
+                // Get variables that we will use often
+                let heroName = document.getElementById('hero-id').value.toLowerCase();
+                let xmenTemplate = document.getElementById('tpl-hero');
+                let xmenTemplateClone = xmenTemplate.content.cloneNode(true);
+                let heroToBeShown = null;
 
-            .then(function (data) {
-
-                let temp = document.getElementById("tpl-hero");
-                //make a copy of the html content so you can paste it in the target
-
-                for (let i = 0; i < data.heroes.length; i++) {
-
-                    //puts the array items in the innerhtml of the copy
-                    let copy = temp.content.cloneNode(true);
-
-
-                    // get lists from json
-                    copy.querySelector(".name").innerHTML = data.heroes[i].name;
-                    copy.querySelector(".alter-ego").innerHTML = data.heroes[i].alterEgo;
-                    copy.querySelector(".powers").innerHTML = data.heroes[i].abilities;
-
-                    document.getElementById("target").appendChild(copy);
+                // set hero to be shown as a vairbale
+                for (let hero in heroList.heroes) {
+                    if (heroName === heroList.heroes[hero].name.toLowerCase()) {
+                        heroToBeShown = heroList.heroes[hero];
+                        console.log(heroToBeShown);
+                    }
                 }
 
-            })
-    });
+                // error check
+                if (heroToBeShown === null) {
+                    alert('choose from: Cyclops, Marvel Girl, Beast, Angel, or Iceman');
+                    return;
+                }
 
+                // using the clone to set inner HTML
+                xmenTemplateClone.querySelector('.name').innerHTML = heroToBeShown.name + '</br>';
+                xmenTemplateClone.querySelector('.alter-ego').innerHTML = heroToBeShown.alterEgo;
+                xmenTemplateClone.querySelector('.powers').innerHTML += '<ul style="text-decoration: underline; padding-right: 50px;">Powers';
+                for (let i = 0; i < heroToBeShown.abilities.length; i++) {
+                    xmenTemplateClone.querySelector('.powers').innerHTML += '<li>' + heroToBeShown.abilities[i] + '</li>';
+                }
+                xmenTemplateClone.querySelector('.powers').innerHTML += '</ul>';
+
+                document.getElementById('target').appendChild(xmenTemplateClone);
+
+            });
+    });
 })();
