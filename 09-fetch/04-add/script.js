4da7ba3d4aa5c7ce10409680e05e69b778682fb0
moeti <moetichentuf@hotmail.com>
2020-01-09 13:27:49 +0100

    still don't fully understand it, going to do part 9 again on the weekend

--- 09-fetch/04-add/script.js
+++ 09-fetch/04-add/script.js
@@ -10,5 +10,34 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    (() => {
+        // your code here
+        let hero = {}; // empty array for multiple elements;
+        fetch('../../_shared/api.json')
+
+            .then(function (link) {
+                return link.json();
+            })
+
+            .then(function (data) {
+
+                document.getElementById("run").addEventListener("click", function() {
+
+                    let newName = document.getElementById("hero-name").value;
+                    let newEgo = document.getElementById("hero-alter-ego").value;
+                    let newPowers = document.getElementById("hero-powers").value;
+
+                    hero ["id"] = data.heroes.length +1;
+                    hero ["name"] = newName;
+                    hero ["alterEgo"] = newEgo;
+                    hero ["abilities"] = []; /*array in array */
+
+                    hero.abilities.push(newPowers);
+                    //push the new hero in the array
+                    data.heroes.push(hero);
+                    console.log(data.heroes)
+                })
+
+            })
+    })();
 })();
--- 09-fetch/05-delete/script.js
+++ 09-fetch/05-delete/script.js
@@ -9,6 +9,26 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(() => {
-    // your code here
-})();
+
+    (() => {
+
+        fetch('../../_shared/api.json')
+            .then(function (link) {
+                return link.json();
+            })
+            .then(function (data) {
+                document.getElementById("run").addEventListener("click", function() {
+                    // get the id from the form
+                    let input = document.getElementById("hero-id").value;
+                    // use splice to remove element
+
+
+                    //use findIndex to show first element where hero.id is equal to the input
+                    data.heroes.splice(data.heroes.findIndex(function split (hero) {
+                        return hero.id == input;
+                    }),1);
+                    console.log(data);
+                });
+            })
+    })();
+
